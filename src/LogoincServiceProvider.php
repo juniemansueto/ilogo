501b4f9b502d64c1e51f86ae0e82a6db28af33e8
juniemansueto <juniecares@gmail.com>
2020-06-08 15:55:52 +0800

    remove logoinc hooks

--- src/LogoincServiceProvider.php
+++ src/LogoincServiceProvider.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\View;
 use Illuminate\Support\Str;
 use Intervention\Image\ImageServiceProvider;
 use Larapack\DoctrineSupport\DoctrineSupportServiceProvider;
-use Ilogo\LogoincHooks\LogoincHooksServiceProvider;
 use ILOGO\Logoinc\Events\FormFieldsRegistered;
 use ILOGO\Logoinc\Facades\Logoinc as LogoincFacade;
 use ILOGO\Logoinc\FormFields\After\DescriptionHandler;
