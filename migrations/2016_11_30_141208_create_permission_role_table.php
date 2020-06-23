174a21eee1aea2e574dc8a28590d0b677415e24a
juniemansueto <juniecares@gmail.com>
2020-06-23 09:47:45 +0800

    remove

--- migrations/2016_11_30_141208_create_permission_role_table.php
+++ migrations/2016_11_30_141208_create_permission_role_table.php
@@ -13,13 +13,14 @@ class CreatePermissionRoleTable extends Migration
      */
     public function up()
     {
-        Schema::create('permission_role', function (Blueprint $table) {
+      /*  Schema::create('permission_role', function (Blueprint $table) {
             $table->bigInteger('permission_id')->unsigned()->index();
             $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
             $table->bigInteger('role_id')->unsigned()->index();
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
             $table->primary(['permission_id', 'role_id']);
         });
+        */
     }
 
     /**
@@ -29,6 +30,6 @@ class CreatePermissionRoleTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('permission_role');
+      //  Schema::dropIfExists('permission_role');
     }
 }
